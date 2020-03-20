67aaa4cde442a5e4abba8fe83a82255f37d56bc1
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-03-20 16:10:52 +0900

    add index page
    レポジトリ一覧表示のためのindexページ作成

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Index() {
+  return (
+    <React.Fragment>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Boby</th>
+        </tr>
+        <tr>
+          <td>
+            test1
+          </td>
+          <td>
+            test2
+          </td>
+          <td>
+            test3
+          </td>
+        </tr>
+      </table>
+
+    </React.Fragment>
+  );
+}
+
+export default Index;
